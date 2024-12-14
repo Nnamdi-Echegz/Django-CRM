d900aea0835991321fa43294254dbd268ea3b0d0
Nnamdi Echegini <nnamdiechegini@gmail.com>
2024-12-14 00:28:26 -0500

    authentication and registration logic

--- CRMwebsite/views.py
+++ CRMwebsite/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check to see if user is logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful !")
+            return redirect('home')
+        else:
+            messages.success(request, "We encountered an error logging you in, please ensure your details are correct")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+#def login_user(request):
+    #pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have logged out of this application")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
